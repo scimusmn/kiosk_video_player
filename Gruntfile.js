d95e890c36d5ba795bdbaa0f0b776a006a98ea32
Sarah German <sgerman@smm.org>
2013-07-29 18:16:43 -0500

    Removed component-specific code from master branch
    Anything specific to one video player component should go onto that
    component's branch.

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,19 +6,14 @@ module.exports = function(grunt) {
     uglify: {
         kiosk_scripts: {
           files: {
-            'build/js/potter_player.min.js': [
-              'libs/jquery-2.0.3.min.js',
-              'libs/video.js',
-              'src/js/video_kiosk.js',
-              'src/js/screensaver.js',
-            ]
+            // Define the input and output files here
           }
         }
     },
     cssmin: {
       compress: {
         files: {
-          'build/css/potter_player.min.css': ['src/css/reset.css', 'src/css/colorado.css', 'src/css/build.css']
+          // Define the input and output files here
         }
       }
     },
