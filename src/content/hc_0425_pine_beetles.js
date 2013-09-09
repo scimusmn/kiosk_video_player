1783293ecd27f01994de0b6741a060c37708a9fe
Sarah German <sgerman@smm.org>
2013-09-09 13:48:19 -0500

    Added content object: HC 0425 Pine Beetles

---
+++ src/content/hc_0425_pine_beetles.js
@@ -0,0 +1,12 @@
+var content = {
+  page_title: "Choose a video",
+  videos: [
+    {
+      title : "How do pine beetles kill trees?",
+      subtitle : "Running time: 1:30"
+    },
+    {
+      title : "Why is the infestation so bad?",
+      subtitle : "Running time: 1:30"
+   ]
+}
