ee04b58010db37605ddfb838c43d4028a1d05c97
Sarah German <sgerman@smm.org>
2013-09-09 15:56:59 -0500

    Added theme/subtheme settings

---
+++ src/content/history_colorado/hc_0425_pine_beetles.js
@@ -0,0 +1,18 @@
+var content = {
+  page_title: "Choose a video",
+  videos: [
+    {
+      title : "How do pine beetles kill trees?",
+      subtitle : "Running time: 1:30"
+    },
+    {
+      title : "Why is the infestation so bad?",
+      subtitle : "Running time: 1:30"
+    }
+  ]
+}
+
+var config = {
+  theme: "history_colorado",
+  subtheme: "hc_snow"
+}
