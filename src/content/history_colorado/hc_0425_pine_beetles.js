3f27cc002a21bd7757f4fdcf921d92fcab3e7c4b
Sarah German <sgerman@smm.org>
2013-09-09 16:34:31 -0500

    Combined config and content into one master object
    Now we can reference the component ID and theme from the template. Using
    this for media asset file paths.

--- src/content/history_colorado/hc_0425_pine_beetles.js
+++ src/content/history_colorado/hc_0425_pine_beetles.js
@@ -1,18 +1,21 @@
-var content = {
-  page_title: "Choose a video",
-  videos: [
-    {
-      title : "How do pine beetles kill trees?",
-      subtitle : "Running time: 1:30"
-    },
-    {
-      title : "Why is the infestation so bad?",
-      subtitle : "Running time: 1:30"
-    }
-  ]
+var kiosk = {
+  content: {
+    page_title: "Choose a video",
+    videos: [
+      {
+        title : "How do pine beetles kill trees?",
+        subtitle : "Running time: 1:30"
+      },
+      {
+        title : "Why is the infestation so bad?",
+        subtitle : "Running time: 1:30"
+      }
+    ]
+  },
+  config: {
+    theme: "history_colorado",
+    subtheme: "hc_snow",
+    component_id: "0425"
+  }
 }
 
-var config = {
-  theme: "history_colorado",
-  subtheme: "hc_snow"
-}
