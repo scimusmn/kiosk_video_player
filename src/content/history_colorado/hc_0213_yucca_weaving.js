8ec768231d9145100acb89cfe162142c2414f7df
Sarah German <sgerman@smm.org>
2013-09-09 22:54:51 -0500

    Added content file for 0213 - yucca weaving

---
+++ src/content/history_colorado/hc_0213_yucca_weaving.js
@@ -0,0 +1,25 @@
+var kiosk = {
+  content: {
+    page_title: "Choose a video",
+    videos: [
+      {
+        title : "Meet a pueblo craftsman",
+        subtitle : "Running time: 1:33"
+      },
+      {
+        title : "Preparing yucca fibers",
+        subtitle : "Running time: 1:30"
+      },
+        titie : "Making cordage",
+        subtitle : "Runnig time: 1:30"
+      }
+    ]
+  },
+  config: {
+    theme: "history_colorado",
+    subtheme: "hc_brown_wide",
+    component_id: "0213"
+  }
+}
+
+
