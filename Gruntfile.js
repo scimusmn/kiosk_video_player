b3f43530ed33ef02a3c09b73e1cd739a95e6a250
Sarah German <sgerman@smm.org>
2013-09-10 11:36:32 -0500

    Updated gruntfile for 0425
    Would like to make this more generic in the future, like pulling from
    the config vars in the content file

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,22 +4,34 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     uglify: {
-        kiosk_scripts: {
-          files: {
-            // Define the input and output files here
-          }
+      kiosk_scripts: {
+        files: {
+          'build/js/kiosk_video_player.js': [
+            'libs/jquery-2.0.3.min.js',
+            'libs/video.js',
+            'libs/handlebars.js',
+            'src/js/video_kiosk.js',
+            'src/js/screensaver.js',
+            'src/content/history_colorado/hc_0425_pine_beetles.js'
+          ]
         }
+      }
     },
     cssmin: {
       compress: {
         files: {
-          // Define the input and output files here
+          'build/theme/css/kiosk_video_player.css': [
+            'src/themes/base/reset.css',
+            'src/themes/base/video_kiosk.css',
+            'src/themes/history_colorado/colorado.css',
+            'src/themes/base/build.css'
+          ]
         }
       }
     },
     smushit: {
       path: {
-        src:['assets/images']
+        src:['assets/images', 'assets/themes']
       },
     }
   });
