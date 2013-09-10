4e70c21cf1a75bdd14efd583a37eaaeddcfd04f1
Sarah German <sgerman@smm.org>
2013-09-09 22:47:45 -0500

    Added paint a pot content file

---
+++ src/content/history_colorado/hc_0214_paint_pot.js
@@ -0,0 +1,25 @@
+var kiosk = {
+  content: {
+    page_title: "Choose a video",
+    videos: [
+      {
+        title : "Meet the potter",
+        subtitle : "Running time: 1:33"
+      },
+      {
+        title : "Starting the pot",
+        subtitle : "Running time: 1:30"
+      },
+        titie : "Finishing the pot",
+        subtitle : "Runnig time: 1:22"
+      }
+    ]
+  },
+  config: {
+    theme: "history_colorado",
+    subtheme: "hc_brown",
+    component_id: "0214"
+  }
+}
+
+
